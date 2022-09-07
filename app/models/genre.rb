86cdaa0a163770a548016e31f51d6a22dff4b6b8
Tyler <toygrrr@gmail.com>
2022-09-07 12:41:52 -0700

    done

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,25 @@
 class Genre < ActiveRecord::Base
   has_many :songs
   has_many :artists, through: :songs
+  
 
   def song_count
     # return the number of songs in a genre
+    self.songs.length
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.length
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    
+    #this works too
+    #Artist.all.map{|artist| artist.name}
+    
+    #prefer this
+    Artist.all.pluck(:name)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
@@ -10,5 +11,12 @@ class Song < ActiveRecord::Base
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if Drake is *not found*
 
+    #creating drake instance to use to update the song's id connection
+    #READ MORE ABOUT THIS
+    drake = Artist.find_or_create_by(name: "Drake")
+    # even though there isnt an artist symbol, update finds artist by read/write for artist_id ect.
+    self.update(artist: drake)
+    #this works too
+    #self.update(artist_id: drake.id)
   end
 end
